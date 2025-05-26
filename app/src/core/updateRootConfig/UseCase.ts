aff51cee59af5271f4c50a8d2b9330fe2109f2ce
Junplid <ousadojunplid@gmail.com>
2025-05-26 08:24:42 -0300

    up

--- app/src/core/updateRootConfig/UseCase.ts
+++ app/src/core/updateRootConfig/UseCase.ts
@@ -1,7 +1,6 @@
 import { resolve } from "path";
 import { UpdateRootConfigDTO_I } from "./DTO";
 import { readFile, writeFile } from "fs-extra";
-import { ApiAssas } from "../../services/Assas/api";
 
 interface RootJSON {
   "token-asaas": string;
@@ -21,18 +20,6 @@ export class UpdateRootConfigUseCase {
     const path = resolve(__dirname, "../../config/root.json");
     const rootJSON: RootJSON = JSON.parse((await readFile(path)).toString());
     writeFile(path, JSON.stringify(Object.assign(rootJSON, dto)));
-
-    try {
-      if (dto["endpoint-asaas"]) {
-        ApiAssas.defaults.url = dto["endpoint-asaas"];
-        ApiAssas.defaults.baseURL = dto["endpoint-asaas"];
-      }
-      if (dto["token-asaas"]) {
-        ApiAssas.defaults.headers.common.access_token = dto["token-asaas"];
-      }
-    } catch (error) {
-      console.log(error);
-    }
 
     return { message: "OK!", status: 201 };
   }
