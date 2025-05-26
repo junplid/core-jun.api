630ecfecac41b4a1072d18eca09b8dffacd835f4
Junplid <ousadojunplid@gmail.com>
2025-05-26 10:26:17 -0300

    up

--- app/main.js
+++ app/main.js
@@ -5,17 +5,8 @@ const { exit } = require("process");
 
 (async () => {
   try {
-    const fileRoot = resolve(__dirname, "./src/config");
-    if (!pathExistsSync(fileRoot)) {
-      await ensureDir(fileRoot);
-      await writeFile(
-        fileRoot + "/root.json",
-        `{"token-asaas": "", "endpoint-asaas": "", "host": "", "port": 0, "secure": false, "authUser": "", "authPass": "", "email": ""}`
-      );
-    }
-
-    if (!pathExistsSync(resolve(__dirname, "./src/bin/connections.json"))) {
-      await writeFile(resolve(__dirname, "./src/bin/connections.json"), `[]`);
+    if (!pathExistsSync(resolve(__dirname, "./bin/connections.json"))) {
+      await writeFile(resolve(__dirname, "./bin/connections.json"), `[]`);
     }
 
     await ensureDir(resolve(__dirname, "./static/audio"));
@@ -23,7 +14,7 @@ const { exit } = require("process");
     await ensureDir(resolve(__dirname, "./static/image"));
     await ensureDir(resolve(__dirname, "./static/pdf"));
     await ensureDir(resolve(__dirname, "./static/video"));
-    await ensureDir(resolve(__dirname, "./src/bin/chatbot-queue"));
+    await ensureDir(resolve(__dirname, "./bin/chatbot-queue"));
   } catch (errors) {
     error(errors, 44);
     exit(2);
