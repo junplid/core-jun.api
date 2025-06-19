eec73a9ad4779b0cfbeaa270a0104a0db92cb1b6
Junplid <ousadojunplid@gmail.com>
2025-06-19 19:18:34 -0300

    up

--- app/tsup.config.ts
+++ app/tsup.config.ts
@@ -1,13 +1,27 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["src/infra/express/index.ts", "prisma/seed.ts"],
-  target: "node21",
-  platform: "node",
-  format: ["cjs"],
-  sourcemap: false,
-  minify: true,
-  external: ["@prisma/client", ".prisma/client", "bcrypt"],
-  dts: false,
-  clean: true,
-});
+export default defineConfig([
+  {
+    entry: ["src/infra/express/index.ts"],
+    target: "node21",
+    platform: "node",
+    format: ["cjs"],
+    sourcemap: false,
+    minify: true,
+    external: ["@prisma/client", ".prisma/client", "bcrypt"],
+    dts: false,
+    clean: true,
+  },
+  {
+    entry: ["prisma/seed.ts"],
+    outDir: "prisma", // => gera prisma/seed.js
+    target: "node21",
+    platform: "node",
+    format: ["cjs"],
+    sourcemap: false,
+    minify: true,
+    external: ["@prisma/client"],
+    dts: false,
+    clean: false, // não apaga a pasta dist recém-criada
+  },
+]);
