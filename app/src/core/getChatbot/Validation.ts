25ae6be928b0c1ebcda4cb500c3188f69653285d
junplid <ousadojunplid@gmail.com>
2026-02-01 21:09:08 -0300

    fix validation of getchatbot

--- app/src/core/getChatbot/Validation.ts
+++ app/src/core/getChatbot/Validation.ts
@@ -5,16 +5,15 @@ import { Joi } from "express-validation";
 export const getChatbotValidation = (
   req: Request<GetChatbotParamsDTO_I, any, GetChatbotDTO_I>,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   const schemaValidation = Joi.object({
-    accountId: Joi.number().required(),
     id: Joi.number().required(),
   });
 
   const validation = schemaValidation.validate(
     { ...req.body, ...req.params },
-    { abortEarly: false }
+    { abortEarly: false },
   );
 
   if (validation.error) {
@@ -27,6 +26,7 @@ export const getChatbotValidation = (
   }
 
   req.params.id = Number(req.params.id);
+  req.body.accountId = req.user?.id!;
 
   next();
 };
